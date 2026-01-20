f566ac8930fc3586b5192a7dfd6fa97375320108
madara uchiha <notwhiten444@gmail.com>
2026-01-20 10:07:40 +0530

    --

---
+++ print.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 0 ;
+    char c='a';
+    float f=14.23;
+    cout<<"the value of a is "<<a<<endl<<"the value of c is "<<c<<endl<<"the value of f is "<<f;
+    return 0;
+}
