7f5d006cd9f55d7d37a7bc67cd98aef3d854a8e9
madara uchiha <notwhiten444@gmail.com>
2026-02-10 10:47:20 +0530

    -

--- moogexam.cpp
+++ moogexam.cpp
@@ -1,18 +1,9 @@
 #include<iostream>
 using namespace std;
-float result(int a,int b){
-return (a+b);
-}
-float result(int a,int b,int c){
-return (a+b+c);
-}
-float result(int a,int b,int c,int d){
-return (a+b+c+d);
-}
-float result(int a,int b,int c,int d,int e){
+float result(int a,int b,int c=0,int d=0,int e=0){
 return (a+b+c+d+e);
 }
 int main(){
-    cout<<result(12,27,23,23,23);
+    cout<<result(12,27);
 return 0;
 }
